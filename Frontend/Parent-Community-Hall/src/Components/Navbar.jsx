53af742a5c49fff6b569acaa1fd23d4d8f500141
Apex <arpana036@gmail.com>
2025-11-15 23:06:17 +0530

    readme.md file created

--- Frontend/Parent-Community-Hall/src/Components/Navbar.jsx
+++ Frontend/Parent-Community-Hall/src/Components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = ({name,filterBy,setFilterBy,setAskName,showPostForm}) => {
   return (
     <nav className='flex w-full md:h-[12%] h-[9%] md:p-3  justify-between text-white items-center'>
       <div className='flex h-full md:gap-[2rem] gap-[0.5rem] md:px-0 px-1 items-center'>
-       <img src="" alt=""  className='bg-white md:h-[4rem] h-[2rem] md:w-[4rem] w-[3rem] rounded-full'/>
+       <img src="" alt=""  className='bg-white md:h-[4rem] h-[2rem] md:w-[4rem] w-[2rem] rounded-full'/>
         <span className='md:text-2xl text-sm'>{name?(<div><span className='md:text-[2.5rem] text-[1.2rem] font-semibold'>Welcome</span> <span className='font-medium text-orange-400'> {name}</span></div>):<span><button className='bg-orange-500 md:p-3 p-1 rounded-xl border border-white font-bold md:text-lg text-sm cursor-pointer' onClick={()=>setAskName(true)}>Click</button> to set your name</span>}</span>
       </div>
       {window.innerWidth>475?<div><span className='text-md text-center'>Arrange <b className='text-orange-400 md:text-xl'>Tweets</b> by </span> <FilterPosts filterBy={filterBy} setFilterBy={setFilterBy}/></div>:<></>}
