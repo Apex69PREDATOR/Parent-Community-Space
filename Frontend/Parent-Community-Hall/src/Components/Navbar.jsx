3636459d0b6edc0f8ef5b262dd9e853fc1306ed3
Apex <arpana036@gmail.com>
2025-11-15 23:03:12 +0530

    readme.md file created

--- Frontend/Parent-Community-Hall/src/Components/Navbar.jsx
+++ Frontend/Parent-Community-Hall/src/Components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = ({name,filterBy,setFilterBy,setAskName,showPostForm}) => {
        <img src="" alt=""  className='bg-white md:h-[4rem] h-[2rem] md:w-[4rem] w-[3rem] rounded-full'/>
         <span className='md:text-2xl text-sm'>{name?(<div><span className='md:text-[2.5rem] text-[1.2rem] font-semibold'>Welcome</span> <span className='font-medium text-orange-400'> {name}</span></div>):<span><button className='bg-orange-500 md:p-3 p-1 rounded-xl border border-white font-bold md:text-lg text-sm cursor-pointer' onClick={()=>setAskName(true)}>Click</button> to set your name</span>}</span>
       </div>
-      <div><span className='text-md text-center'>Arrange <b className='text-orange-400 md:text-xl'>Tweets</b> by </span> <FilterPosts filterBy={filterBy} setFilterBy={setFilterBy}/></div>
+      <div><span className='text-md text-center'>Arrange <b className='text-orange-400 md:text-xl'>Tweets</b> by </span> {window.innerWidth>475?<FilterPosts filterBy={filterBy} setFilterBy={setFilterBy}/>:null}</div>
         <button className='bg-orange-500 md:p-3 p-1 rounded-xl md:mr-[3rem] mr-[0.5rem] border border-white font-bold md:text-lg text-sm cursor-pointer' onClick={()=>showPostForm(true)}>+ Upload Tweet</button>
     </nav>
   )
